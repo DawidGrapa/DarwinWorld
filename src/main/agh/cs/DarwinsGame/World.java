550e82583d357265564a05bf08612d9a5520fe90
DAWID <dgrapa55@gmail.com>
2020-12-12 02:59:35 +0100

    Poprawki bledow

--- src/main/agh/cs/DarwinsGame/World.java
+++ src/main/agh/cs/DarwinsGame/World.java
@@ -7,7 +7,7 @@ import View.GameMainFrame;
 public class World {
     public static void main(String[] args) {
         for(int i=0;i<1;i++){
-            GameMainFrame gameMainFrame = new GameMainFrame(new Simulation(20,20,50,30,20,1,50,10));
+            GameMainFrame gameMainFrame = new GameMainFrame(new Simulation(5,5,2,30,20,1,50,100));
             gameMainFrame.startSimulation();
         }
     }
